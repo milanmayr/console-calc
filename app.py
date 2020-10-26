44fe2df4b31c7d9f7c2a792fb0510cffc55a06d3
Milan May'r <mayrmilan@gmail.com>
2020-10-26 13:20:06 -0500

    Added main python file

---
+++ app.py
@@ -0,0 +1,29 @@
+if __name__ == '__main__':
+    # Introduce app
+    print("This app is a super basic calculator.")
+
+    # Prompt for first number
+    print("\nEnter the first number: ")
+    first_number = int(input())
+
+    # Explain operator options, then prompt for operator
+    print("\n----")
+    print("----")
+    print(" + for addition")
+    print(" - for subtraction")
+    print(" * for multiplication")
+    print(" / for division")
+    print("----")
+    print("----\n")
+    print("Enter an operator: ")
+    operator = input()
+
+    # Prompt for second number
+    print("\nEnter the second number: ")
+    second_number = int(input())
+
+    # Calculate result
+    result = calculate(first_number, second_number, operator)
+
+    # Show result
+    print('\n' + str(first_number) + ' ' + operator + ' ' + str(second_number) + ' = ' + str(result))
